05713c154adfb30e3d5b94dfc757ce80be814e4f
Charles Wan <yty.charles@gmail.com>
2016-09-28 23:16:43 -0400

    Add CreateGroupActivity EditText boxes and onClickListener

--- CompFoodie/app/src/main/java/cs/tufts/edu/compfoodie/CreateGroupActivity.java
+++ CompFoodie/app/src/main/java/cs/tufts/edu/compfoodie/CreateGroupActivity.java
@@ -2,12 +2,25 @@ package cs.tufts.edu.compfoodie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class CreateGroupActivity extends AppCompatActivity {
+public class CreateGroupActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText cuisine_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_group);
+
+        // TEST SPACE
+        cuisine_input = (EditText)findViewById(R.id.cuisine_input);
+
+        cuisine_input.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        ((EditText)v).setText("");
     }
 }
