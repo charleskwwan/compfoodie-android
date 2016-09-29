161fe116315f4373ec06fb13f0ae1e304497c6c3
Charles Wan <yty.charles@gmail.com>
2016-09-28 23:35:43 -0400

    Change onClickListener events to hints for CreateGroup EditTexts

--- CompFoodie/app/src/main/java/cs/tufts/edu/compfoodie/CreateGroupActivity.java
+++ CompFoodie/app/src/main/java/cs/tufts/edu/compfoodie/CreateGroupActivity.java
@@ -5,22 +5,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-public class CreateGroupActivity extends AppCompatActivity implements View.OnClickListener {
+public class CreateGroupActivity extends AppCompatActivity {
     private EditText cuisine_input;
+    private EditText location_input;
+    private EditText party_size_input;
+    private EditText wait_time_input;
+    private EditText short_message_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_group);
 
-        // TEST SPACE
         cuisine_input = (EditText)findViewById(R.id.cuisine_input);
+        location_input = (EditText)findViewById(R.id.location_input);
+        party_size_input = (EditText)findViewById(R.id.party_size_input);
+        wait_time_input = (EditText)findViewById(R.id.wait_time_input);
+        short_message_input = (EditText)findViewById(R.id.short_message_input);
 
-        cuisine_input.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        ((EditText)v).setText("");
+        // TEST SPACE
     }
 }
