8812faa747a8efe7041c51648f65bdb7f6d20dcb
Charles Wan <yty.charles@gmail.com>
2016-10-06 14:36:35 -0400

    Add text fields, time picker, and create button to CreateGroup activity

--- CompFoodie/app/src/main/java/cs/tufts/edu/compfoodie/CreateGroupActivity.java
+++ CompFoodie/app/src/main/java/cs/tufts/edu/compfoodie/CreateGroupActivity.java
@@ -1,24 +1,33 @@
 package cs.tufts.edu.compfoodie;
 
+import android.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class CreateGroupActivity extends AppCompatActivity {
-    private Toolbar toolbar;
-    private BottomMenuBar bottombar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_group);
 
+        BottomMenuBar bottombar = new BottomMenuBar(this, savedInstanceState);
         // ToolBar
-        toolbar = (Toolbar)findViewById(R.id.create_group_toolbar);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.create_group_toolbar);
         toolbar.setTitle(getString(R.string.create_group_title));
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); // need to set parent in manifest
-
-        bottombar = new BottomMenuBar(this, savedInstanceState);
+        // Wait Time Picker
+        ImageButton wtbutton = (ImageButton)findViewById(R.id.wait_time_pick_button);
+        wtbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WaitTimePickerFragment wtpfrag = new WaitTimePickerFragment();
+                wtpfrag.show(getFragmentManager(), "wait_time_picker_fragment");
+            }
+        });
     }
 }
---
+++ CompFoodie/app/src/main/java/cs/tufts/edu/compfoodie/WaitTimePickerFragment.java
@@ -0,0 +1,37 @@
+package cs.tufts.edu.compfoodie;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+import java.util.Locale;
+
+/**
+ * Created by charlw on 10/4/16.
+ *      represents dialog fragment for picking wait time
+ */
+
+public class WaitTimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar cal = Calendar.getInstance();
+        int hourOfDay = cal.get(Calendar.HOUR);
+        int minOfDay = cal.get(Calendar.MINUTE);
+        TimePickerDialog tpd = new TimePickerDialog(getActivity(), this, hourOfDay, minOfDay,
+                                                    DateFormat.is24HourFormat(getActivity()));
+        return tpd;
+    }
+
+    @Override
+    public void onTimeSet(TimePicker tp, int hour, int minute) {
+        TextView wtoutput = (TextView)getActivity().findViewById(R.id.wait_time_output);
+        String time_format = "";
+        wtoutput.setText(String.format(Locale.ENGLISH, "%d:%01d", hour, minute));
+    }
+}
